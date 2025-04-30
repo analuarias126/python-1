5fa885dec1ac878a0fb2363fa4afaf04e45b39d6
anaarias126 <99105569+analuarias126@users.noreply.github.com>
2025-04-29 19:14:57 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,4 @@
-import discord
-from discord.ext import commands
-from bot_logic import gen_pass
-
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"Conectado como {bot.user}")
-
-@bot.command()
-async def password(ctx, length: int = 10):
-    pwd = gen_pass(length)
-    await ctx.send(f"Tu contraseña es: `{pwd}`")
-
-bot.run("TOKEN_AQUI")
+settings = {
+    "prefix": ">",
+    "TOKEN": "THE TOKEN WILL BE STORED HERE"
+}
